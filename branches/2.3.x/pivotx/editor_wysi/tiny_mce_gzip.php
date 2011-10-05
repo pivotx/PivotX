819c9e617cabfad66e2bc80964736f6e839b003c
pivotlog <pivotlog@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2011-10-05 14:33:36 +0000

    Updated TinyMCE to 3.4.6 for PivotX 2.3.x

--- branches/2.3.x/pivotx/editor_wysi/tiny_mce_gzip.php
+++ branches/2.3.x/pivotx/editor_wysi/tiny_mce_gzip.php
@@ -13,7 +13,7 @@
     // Set the error reporting to minimal.
     @error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
-    $build = "3.4.5";
+    $build = "3.4.6";
 
     // Get input
     $plugins = explode(',', getParam("plugins", ""));
