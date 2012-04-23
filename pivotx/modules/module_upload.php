379c1e2b8f41549a03001aeb44eee2eab8215393
Harm <1517188+Harm10@users.noreply.github.com>
2012-04-23 14:19:45 +0000

    Fix 4155 from the trunk to the branch.

--- pivotx/modules/module_upload.php
+++ pivotx/modules/module_upload.php
@@ -40,18 +40,25 @@ class UploadElement {
             $ini_size = strtolower(ini_get('upload_max_filesize'));
             if (strpos('kmg',substr($ini_size,-1)) !== false) {
                 $ini_size .= 'b';  // add a "b" to get kb/mb/gb
+                $ini_unit = substr($ini_size,-1) . 'b';
             }
             if (is_numeric($ini_size)) {  
-                $ini_size = floor($ini_size/1024).'kb'; // changed to kb from k
+                $ini_size = floor($ini_size/1024).'kb';
+                $ini_unit = 'kb';
             }
             // get config option and compare to server value
             $upl_size = getDefault($PIVOTX['config']->get('upload_max_filesize'), -1);
             if ($upl_size > 0) {
-                $ini_unit = substr($ini_size,-2);
-                if ($ini_unit == 'gb') { $upl_size = floor($upl_size/1024/1024/1024).'gb'; }
-                if ($ini_unit == 'mb') { $upl_size = floor($upl_size/1024/1024).'mb'; }
-                if ($ini_unit == 'kb') { $upl_size = floor($upl_size/1024).'kb'; }
-                if (substr($upl_size,0,-2) < substr($ini_size,0,-2)) { $ini_size = $upl_size; }
+                if ($ini_unit == 'gb') { 
+                    $upl_size = floor($upl_size/1024/1024/1024).'gb'; 
+                } else if ($ini_unit == 'mb') { 
+                    $upl_size = floor($upl_size/1024/1024).'mb'; 
+                } else { 
+                    $upl_size = floor($upl_size/1024).'kb'; 
+                }
+                if (substr($upl_size,0,-2) < substr($ini_size,0,-2)) { 
+                    $ini_size = $upl_size; 
+                }
             }
             $params['max_file_size'] = $ini_size;
         }
