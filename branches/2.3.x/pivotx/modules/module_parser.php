7521cb87e909f471e25e0cc8005eaab02cd4a5cb
Harm10 <Harm10@a2bfabd2-e71a-0410-9f27-bcdfd5ae8e08>
2013-03-31 10:42:46 +0000

    Reverting fix 4254 (canonical_always_url)

--- branches/2.3.x/pivotx/modules/module_parser.php
+++ branches/2.3.x/pivotx/modules/module_parser.php
@@ -1152,22 +1152,6 @@ class Parser {
                     $PIVOTX['paths']['canonical_host'],              
                     (empty($link) ? $PIVOTX['paths']['site_url'] : $link)
                 );
-
-            // Canonical link always the same as URL?
-            if (!isset($this->modifier['canonical_link']) && $PIVOTX['config']->get('canonical_always_url')==1 ) {
-                // is there already a function that gets it and can be used here?
-                $pageURL = 'http';
-                if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-                $pageURL .= "://";
-                if ($_SERVER["SERVER_PORT"] != "80") {
-                    $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
-                } else {
-                    $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
-                }
-                $canonical = sprintf("\t<link rel=\"canonical\" href=\"%s\" />\n",
-                    $pageURL
-                    );
-            }
                 
             $PIVOTX['extensions']->addHook(
                 'after_parse',
